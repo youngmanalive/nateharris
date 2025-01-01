cfc6f4bdd2a5dd7d775f89d502cf45fa5b486ce1
Nate Harris <nathanharris.sf@gmail.com>
2025-01-01 17:20:48 -0500

    show loading

--- src/react/views/project/codesandbox.js
+++ src/react/views/project/codesandbox.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CodeSandbox = ({ src, title, id }) => {
-  return <iframe className='codesandbox' title={title || id} src={src} />;
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <>
+      {loading && <Loading />}
+      <iframe
+        hidden={loading}
+        className="codesandbox"
+        onLoad={() => setLoading(false)}
+        title={title || id}
+        src={src}
+      />
+    </>
+  );
 };
 
 export default CodeSandbox;
+
+const Loading = () => {
+  const [dots, setDots] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev + 1) % 4);
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <p>
+      <i>loading codesandbox{'.'.repeat(dots)}</i>
+    </p>
+  );
+};
